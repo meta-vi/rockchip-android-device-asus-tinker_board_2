a1a119d1e94789d41f4e677614ade2e304a3a48c
YiowHur_Ng <yiowhur_ng@asus.com>
2022-02-22 15:25:22 +0800

    [TinkerConfig] Add TinkerConfig to device.mk
    Change-Id: Ifa6fe3c8abdefbb8c89dd65ac0067c783f8f56f3
    Signed-off-by: Rock Shen <rock_shen@asus.com>

--- device.mk
+++ device.mk
@@ -233,3 +233,6 @@ BOARD_SEPOLICY_DIRS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/cpu_gpu_utility.sh:$(TARGET_COPY_OUT_VENDOR)/bin/cpu_gpu_utility.sh
 
+# ASUS TinkerConfig
+PRODUCT_PACKAGES += \
+    TinkerConfig
