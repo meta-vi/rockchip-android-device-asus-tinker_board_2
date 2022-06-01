df0203cbf12a74d33ffc3d36713302d89ae878be
Rock Shen <rock_shen@asus.com>
2022-06-01 09:15:06 +0800

    Add device WW_Tinker_Board_2
    Clone from device/rockchip/rk3399 and modify it for Tinker Board 2.
    
    Change-Id: If2c18883326aa8a3b2dce8d3309340782f8e5d6a
    Signed-off-by: Rock Shen <rock_shen@asus.com>

---
+++ Tinker_Board_2/AndroidBoard.mk
@@ -0,0 +1,8 @@
+# generate fstab file for device
+-include device/rockchip/common/build/rockchip/RebuildFstab.mk
+
+# generate dtbo image for device
+-include device/rockchip/common/build/rockchip/RebuildDtboImg.mk
+
+# generate parameter.txt for device
+-include device/asus/tinker_board_2/Tinker_Board_2/RebuildParameter.mk
---
+++ Tinker_Board_2/WW_Tinker_Board_2.mk
@@ -0,0 +1,48 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# First lunching is S, api_level is 31
+PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_DTBO_TEMPLATE := $(LOCAL_PATH)/dt-overlay.in
+PRODUCT_BOOT_DEVICE := fe330000.sdhci
+include device/rockchip/common/build/rockchip/DynamicPartitions.mk
+include device/asus/tinker_board_2/Tinker_Board_2/BoardConfig.mk
+include device/rockchip/common/BoardConfig.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/asus/tinker_board_2/device.mk)
+$(call inherit-product, device/rockchip/common/device.mk)
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_NAME := WW_Tinker_Board_2
+PRODUCT_DEVICE := Tinker_Board_2
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Tinker Board 2
+PRODUCT_MANUFACTURER := asus
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_PACKAGES += \
+    SoundRecorder
+
+PRODUCT_PACKAGE_OVERLAYS += device/asus/tinker_board_2/Tinker_Board_2/overlay
+# Get the long list of APNs
+PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/spn-conf.xml:system/etc/spn-conf.xml
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.version = 1.0.0 \
+    ro.product.ota.host = 192.168.1.1:8888 \
+    ro.sf.lcd_density=280
