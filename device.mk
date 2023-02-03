7ab3485213570865ffe6ecd471d8f1e1a266309e
Jason7_Chang <Jason7_Chang@asus.com>
2023-02-03 16:08:29 +0800

    Revert "[FOTA]Add DMClient-V3.1.0.1(1)"
    This reverts commit 45b1a8929131ef61310353c48db57274987b66fd.
    
    Change-Id: I9b9107a737edbc32724813293b1be2d91461003d

--- device.mk
+++ device.mk
@@ -31,10 +31,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RockchipPinnerService
 
-#DMClient
-PRODUCT_PACKAGES += \
-    DMClient
-
 #enable this for support f2fs with data partion
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 
